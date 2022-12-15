01dfc31fa6990dab818f92f0eb57ba1dc14ce73c
programmer0101 <113084773+Yousuf9963@users.noreply.github.com>
2022-12-15 06:03:16 +0300

    Add files via upload

---
+++ prank-hacking.py
@@ -0,0 +1,31 @@
+print("[&] Coded By Yousuf Shafi'i Muhammad Junior Programmer.")
+
+print("")
+
+print("[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog.")
+
+print("")
+print("[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
+
+
+print("I hope for you good future and i am willing that you will come high effort.")
+
+print("")
+
+print("Example : example.com")
+print("")
+hack = input("Enter Target Website :")
+print("")
+
+print("Start attacking........")
+print("Hacking Government System 0%")
+print("Hacking Government System  10%")
+print("Hacking Government System  20%")
+print("Hacking Government System  40%")
+print("Hacking Government System  60%")
+print("Hacking Government System  70%")
+print("Hacking Government System  80%")
+print("Hacking Government System  90%")
+print("Hacking Government System  100%")
+print("Government System  Hacked Successsfully")
+
